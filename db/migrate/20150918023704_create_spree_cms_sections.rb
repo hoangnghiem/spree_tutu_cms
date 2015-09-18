845b9d30ec50ae680b631b8c129ffd49757ee677
hoangnghiem <hoangnghiem1711@gmail.com>
2015-09-18 14:27:57 +0700

    Update migration

--- db/migrate/20150918023704_create_spree_cms_sections.rb
+++ db/migrate/20150918023704_create_spree_cms_sections.rb
@@ -1,7 +1,7 @@
 class CreateSpreeCmsSections < ActiveRecord::Migration
   def change
     create_table :spree_cms_sections do |t|
-      t.references :layout, index: true, foreign_key: true
+      t.references :layout, index: true
       t.string :name
       t.string :key
 
--- db/migrate/20150918045624_create_spree_cms_page_contents.rb
+++ db/migrate/20150918045624_create_spree_cms_page_contents.rb
@@ -1,8 +1,8 @@
 class CreateSpreeCmsPageContents < ActiveRecord::Migration
   def change
     create_table :spree_cms_page_contents do |t|
-      t.references :page, index: true, foreign_key: true
-      t.references :section, index: true, foreign_key: true
+      t.references :page, index: true
+      t.references :section, index: true
       t.text :content
 
       t.timestamps null: false
