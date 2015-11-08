14f4005affc3518e14e74ead82c713291f0f38c4
hoangnghiem <hoangnghiem1711@gmail.com>
2015-11-08 21:05:39 +0700

    Update asset image

--- app/models/spree/cms/asset_image.rb
+++ app/models/spree/cms/asset_image.rb
@@ -2,7 +2,7 @@ class Spree::Cms::AssetImage < ActiveRecord::Base
   has_one :asset, as: :content, class_name: 'Spree::Cms::Asset'
   has_attached_file :photo,
                     styles: { thumb: '100x100>' },
-                    default_style: :thumb,
+                    default_style: :original,
                     url: '/spree/cms/asset_image/:id/:style/:basename.:extension',
                     path: ':rails_root/public/spree/cms/asset_image/:id/:style/:basename.:extension'
 
